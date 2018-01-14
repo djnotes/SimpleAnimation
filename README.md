0cddad6b75319e2458bb9309aebad9c4f31fa78b
Mehdi Haghgoo <mehdihaghgoo2014@gmail.com>
2018-01-14 14:39:58 +0330

    Added license

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # SimpleAnimation: A Lightweight animation library for Android using ConstraintLayout   
 # Sample Output:   
 ![Sample Output](static/sample.gif)
+
+# Usage   
+To use the library you can simply pass a ConstraintLayout along with a secondary layout (where you have defined new constraints) to the the singleton class:   
+
+
+```
+ConstraintLayout layout = findViewById(R.id.layout);
+MyAnimation mAnimation = MyAnimation.getInstance();
+    mAnimation.with(this).from(R.layout.second_keyframe)
+                .animate(layout);
+
+``` 
+
+
--- demo/src/main/java/com/sematec_co/simpleanimation/LauncherActivity.java
+++ demo/src/main/java/com/sematec_co/simpleanimation/LauncherActivity.java
@@ -1,15 +1,11 @@
 package com.sematec_co.simpleanimation;
 
 import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.github.constraintlayoutanimation.MyAnimation;
-import com.github.constraintlayoutanimation.test;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -36,7 +32,6 @@ public class LauncherActivity extends AppCompatActivity{
     @Click
     void start(){
         showAnimation();
-        Log.i(LOG_TAG, "BUTTON clicked");
     }
 
 
